58140ef5075e139ac0d368a90d75258ec226e300
Ryan Allarey <60989959+rallarey@users.noreply.github.com>
2022-04-07 10:20:53 -0400

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# Energy Avenger
-### Ryan Allarey
-> implemented player movement and changes directions. Animations not fully working while holding down movement key, but they do work.
-### Tyler Yates
-> implemented player collision on the borders of game and added shift key and interact parts
-### Nolan Spillane
-> implemented a basic enemy, with random movements
---
+++ dev/precache-manifest.63ca029299f7f307a71a84b9d6b14ade.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0efa7ebcb4d58d7723b90e0e09b24b90",
+    "url": "/github-hosting-directions/devScene1.js"
+  },
+  {
+    "revision": "b0f5d6f20ca11f22cc13cd88f3bbf667",
+    "url": "/github-hosting-directions/devScene2.js"
+  },
+  {
+    "revision": "7e3bff33ce6ef9cdb9dbe98d71a0168a",
+    "url": "/github-hosting-directions/devassets/images/options_button.png"
+  },
+  {
+    "revision": "54daf94985d6510f2a6d8c322b93bebe",
+    "url": "/github-hosting-directions/devassets/images/play_button.png"
+  },
+  {
+    "revision": "6cfc13d11cbce7205ff35fee64e839f6",
+    "url": "/github-hosting-directions/devassets/spritesheets/beam.png"
+  },
+  {
+    "revision": "6be3462bcaa4a183b678800bb66d176c",
+    "url": "/github-hosting-directions/devassets/spritesheets/beam1.png"
+  },
+  {
+    "revision": "7d63219cd108f9d024ad3a2054937a46",
+    "url": "/github-hosting-directions/devassets/spritesheets/enemy1.png"
+  },
+  {
+    "revision": "6bd69b63e360ca70397ac3e52763bee4",
+    "url": "/github-hosting-directions/devassets/spritesheets/player.png"
+  },
+  {
+    "revision": "c94d2f463d6723e0524aefd5013fa200",
+    "url": "/github-hosting-directions/devassets/spritesheets/player2.png"
+  },
+  {
+    "revision": "6e89258d5e7e77976b744e3c67628d48",
+    "url": "/github-hosting-directions/devbeam.js"
+  },
+  {
+    "revision": "609a4dfeb8abea64eb8d4f516b8106ea",
+    "url": "/github-hosting-directions/devenemy1.js"
+  },
+  {
+    "revision": "42c426eda4dba99f9fdd8d5f58f0fe4d",
+    "url": "/github-hosting-directions/devgame.js"
+  },
+  {
+    "revision": "768df2bc7dc816fe4f1c",
+    "url": "/github-hosting-directions/devgen.main.7089e5c2be3222216ec3.bundle.js"
+  },
+  {
+    "revision": "95918db77c005ffb48eadd90b7425e77",
+    "url": "/github-hosting-directions/devindex.html"
+  },
+  {
+    "revision": "8a934669d0d8c660d7a32d9e28fa60dd",
+    "url": "/github-hosting-directions/devmainMenu.js"
+  },
+  {
+    "revision": "d1f5c8aca62fa9b256f467a79dfecfa7",
+    "url": "/github-hosting-directions/devmanifest.json"
+  },
+  {
+    "revision": "5c62495a52b57e3b002d8dca78ffabc9",
+    "url": "/github-hosting-directions/devphaser.min.js"
+  },
+  {
+    "revision": "6a3da97abb26f529fc65fd91a7eae695",
+    "url": "/github-hosting-directions/devplayer.js"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/github-hosting-directions/devsw.js"
+  },
+  {
+    "revision": "e6d79ddba4396b5af0bff5a06be3e482",
+    "url": "/github-hosting-directions/devswitch.js"
+  }
+]);
