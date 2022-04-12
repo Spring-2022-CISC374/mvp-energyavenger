e847bbfbabd72e84d57fce407c2459ba53a17a9d
SeanMcMann <seanmcm@udel.edu>
2022-04-12 16:39:33 -0400

    more background

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
 private background: Phaser.GameObjects.TileSprite;
 this.background = this.add.tileSprite(0, 0, this.scale.width, this.scale.height, "background");
 this.background.setOrigin(0,0);
+this.enemies = [];
+this.player = new Player(this, data.player.x, data.player.y);
+
